ef27a1843754d18ce280cda39691a7e58e482d17
Josh Jordal <josh_jordal@JPC.localdomain>
2023-03-13 16:43:20 -0700

    Fixed resource and schema same name issue

--- api/flask_api.py
+++ api/flask_api.py
@@ -7,7 +7,7 @@ from flask_restful import Api
 from mongoengine import connect
 # You need to implement two resources: Brevet and Brevets.
 # Uncomment when done:
-from resources.brevet import Brevet
+from resources.brevet import BrevetRes
 from resources.brevets import Brevets
 
 # Connect MongoEngine to mongodb
@@ -20,7 +20,7 @@ port_num = True if "PORT" not in os.environ else os.environ["PORT"]
 api = Api(app)
 
 # Bind resources to paths here:
-api.add_resource(Brevet, "/api/brevet/<_id>")
+api.add_resource(BrevetRes, "/api/brevet/<_id>")
 api.add_resource(Brevets, "/api/brevets")
 app.logger.debug("API RAN")
 
--- api/resources/brevet.py
+++ api/resources/brevet.py
@@ -7,7 +7,7 @@ from flask_restful import Resource
 # You need to implement this in database/models.py
 from database.models import Brevet
 
-class Brevet(Resource):
+class BrevetRes(Resource):
     def get(self, _id):
         json_object = Brevet.objects(id=_id).to_json()
         return Response(json_object, mimetype="application/json", status=200)
